bd368252ebfc11e0bf4797dc0ea853f86b63f4d9
James P Bondeson <jpbond58@gmail.com>
2020-08-23 04:43:12 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,18 +1,31 @@
-def help
-puts "I accept the following commands:"
-puts "- help : displays this help message"
-puts "- list : displays a list of songs you can play"
-puts "- play : lets you choose a song to play"
-puts "- exit : exits this program"
+def say_hello(name)
+  "Hi #{name}!"
 end
 
-def play
+puts "Enter your name:"
+users_name = gets.strip
 
-end
+puts say_hello(users_name)
 
-def list
 
-end
 
-def exit_jukebox
-end
+
+
+# def help
+# puts "I accept the following commands:"
+# puts "- help : displays this help message"
+# puts "- list : displays a list of songs you can play"
+# puts "- play : lets you choose a song to play"
+# puts "- exit : exits this program"
+# end
+#
+# def play
+#
+# end
+#
+# def list
+#
+# end
+#
+# def exit_jukebox
+# end
